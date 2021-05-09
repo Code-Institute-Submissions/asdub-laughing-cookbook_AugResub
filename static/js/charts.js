bfcc324748ee1fc96d0555fdb80b35d0c06d2142
asdub <adam.smitzer@gmail.com>
2021-05-09 16:44:28 +0000

    feat: user activity chart added to admin dashboard

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,12 +1,13 @@
-// Admin dashboard map - 
-function chart_chef(vars) {
+// Admin dashboard Charts  - Chart.js config
+
+// Top 5 chefs by recipe chart
+function chart_chef(chef_data) {
     const labels = []
     const data_count = []
-    for ( var i in vars ) {
-        labels.push(vars[i]._id)
-        data_count.push(vars[i].count)
+    for ( var i in chef_data ) {
+        labels.push(chef_data[i]._id)
+        data_count.push(chef_data[i].count)
     }
-    console.log(labels)
     const data = {
         labels: labels,
         datasets: [{
@@ -44,6 +45,56 @@ function chart_chef(vars) {
 
     var myChart = new Chart(
         document.getElementById('chefChart'),
+        config
+    );
+};
+
+// Top 5 users by activity chart
+function chart_user(activity_data) {
+    const labels = []
+    const data_count = []
+    for ( var i in activity_data ) {
+        labels.push(activity_data[i]._id)
+        data_count.push(activity_data[i].total)
+    }
+    console.log(labels)
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Top 5 Active Users',
+            data: data_count,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+                'rgba(255, 205, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgb(255, 99, 132)',
+                'rgb(255, 159, 64)',
+                'rgb(255, 205, 86)',
+                'rgb(75, 192, 192)',
+                'rgb(54, 162, 235)'
+            ],
+            borderWidth: 1
+        }]
+    };
+
+    const config = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                y: {
+                  beginAtZero: true
+                }
+            }
+        }
+    };
+
+    var myChart = new Chart(
+        document.getElementById('userChart'),
         config
     );
 };
