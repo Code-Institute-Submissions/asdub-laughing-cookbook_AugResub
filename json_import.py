164ec49e8bb361633979ee5234087ac6ab1872cd
asdub <adam.smitzer@gmail.com>
2021-05-10 19:23:51 +0000

    Flask debug disabled

--- json_import.py
+++ json_import.py
@@ -18,19 +18,20 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 mongo = PyMongo(app)
 
 # Upload json data to Mongo DB
-# Data found to be incomplete so aggregated for more content rich data. 
+# Data found to be incomplete so aggregated for more content rich data.
+
 
 def load_json_data():
-    Load JSON Data and insert to MongoDB collection
+    # Load JSON Data and insert to MongoDB collection
     data = [json.loads(line) for line in open('data/recipes.json', 'r')]
     if isinstance(data, list):
         mongo.db.recipes.insert_many(data)
         mongo.db.recipes.aggregate([
             {"$match": {"photo_url": {"$exists": True, "$not": {"$type": 10}},
-                    "total_time_minutes": {"$gt": 0},
-                    "description": {"$exists": True},
-                    "$expr": {"$gt": [{"$strLenCP": "$description"}, 80]}
-                }},
+            "total_time_minutes": {"$gt": 0},
+            "description": {"$exists": True},
+            "$expr": {"$gt": [{"$strLenCP": "$description"}, 80]}
+            }},
             {"$out": 'recipes_clean'}
         ])
 
