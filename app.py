303a6ab06396e1273e2ae511c649fb49ec5c5928
asdub <adam.smitzer@gmail.com>
2021-05-10 15:25:11 +0000

    removed letsenrypt function and vars - not possible for free on heroku

--- app.py
+++ app.py
@@ -16,17 +16,8 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["LETSENCRYPT"] = os.environ.get("LETSENCRYPT")
 
 mongo = PyMongo(app)
-
-
-# Letencrypt function to display data at required domain
-@app.route("/.well-known/acme-challenge/t6hXa2gvy263TrRmZr7AbW6evV9W5EbRrBbctD--ezE")
-def ssl():
-    data = app.config["LETSENCRYPT"]
-    return Response(data, mimetype='text/plain')
-
 
 # Home
 @app.route("/")
